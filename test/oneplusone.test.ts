e44a24f51f191eb01188579ffd90d0fcc808a32f
Kamya <peteraksel@live.dk>
2024-10-17 15:17:04 +0200

    initial commit

---
+++ test/oneplusone.test.ts
@@ -0,0 +1,8 @@
+import assert from 'assert'
+import { describe, it } from 'node:test'
+
+describe('Basic calculus', () => {
+    it('1 + 1 = 2', () => {
+        assert.strictEqual(1 + 1, 2)
+    })
+})
