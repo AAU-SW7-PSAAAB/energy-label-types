4b45164cd1e4d38ace5c80a01ac9850ad9acdcf1
Kamya <peteraksel@live.dk>
2024-10-24 12:47:25 +0200

    npm run fmt

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,16 +1,13 @@
 import assert from 'assert'
 import { describe, it } from 'node:test'
 
-import {callConsts} from "../lib/index.js"
-
+import { callConsts } from '../lib/index.js'
 
 describe('Parser', () => {
     it('it can parse a version responce', async () => {
-        const res = new Response('{"version" : "1.0.0"}');
+        const res = new Response('{"version" : "1.0.0"}')
         const version = await callConsts['/version'].parse(res)
 
-        assert.strictEqual(version.version, "1.0.0")
+        assert.strictEqual(version.version, '1.0.0')
     })
 })
-
-
