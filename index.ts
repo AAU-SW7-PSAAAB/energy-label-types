c758c89e01a34e635b2577e3a9b12fe9c03c5c10
Bastian Hansen <bastian.b.hansen@gmail.com>
2024-10-30 10:20:04 +0100

    Formatting, spelling and zod enum implementation

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
-export { Server, version, run } from './lib/index.js'
-export type { Paths, Version, Run } from './lib/index.js'
+export {
+	statusCodeEnum,
+	StatusCodes,
+	Server,
+	version,
+	run,
+} from './lib/index.js'
+export type { Version, Run } from './lib/index.js'
